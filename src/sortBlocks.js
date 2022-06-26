5502980da4f203cf67f24cb75c5010c720852200
Christopher Rieder <christopher.rieder@gmail.com>
2022-06-26 19:28:27 -0300

    adding time measurement for the sorting function

--- src/sortBlocks.js
+++ src/sortBlocks.js
@@ -1,7 +1,9 @@
 const { getBlocks, checkSolution } = require("./api")
 const { check } = require("./check")
 
+// TODO: not sure about this name. is it really sorting?
 async function sortBlocks() {
+    console.time('time')
     const blocks = await getBlocks()
     console.log("blocks", blocks)
     const sortResult = await check(blocks.data)
@@ -10,6 +12,7 @@ async function sortBlocks() {
     const checkResult = await checkSolution(solution.join(''))
     console.log("-".repeat(40))
     console.log("The solution check result is ", checkResult ? 'CORRECT ✔️' : "INCORRECT ❌")
+    console.timeEnd('time')
 }
 
 module.exports = sortBlocks
